e18ce2f774db1aaed137bd8b281555998b972ff6
Liam <liam@scalzulli.com>
2025-09-01 13:47:30 -0400

    Scaffold client application

--- justfile
+++ justfile
@@ -2,10 +2,19 @@ set dotenv-load
 
 export EDITOR := 'nvim'
 
+alias d := dev
 alias f := fmt
 
 default:
   just --list
 
-fmt:
+dev:
+  bun run dev
+
+fmt: fmt-indexer fmt-web
+
+fmt-indexer:
   cargo +nightly fmt
+
+fmt-web:
+  prettier --write .
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  css: { devSourcemap: true },
+  plugins: [react(), tailwindcss()],
+  resolve: { alias: { '@': path.resolve(__dirname, './client/src') } },
+  root: './client',
+});
